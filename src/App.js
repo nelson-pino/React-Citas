1dfe2867cb68aad14e57b2d68baa51bacfe8d706
Nelson Pino <nelson.pino.net@gmail.com>
2021-04-26 20:33:25 -0400

    creacion de citas y validacion de campos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-
-
+import React, { Fragment, useState } from "react";
+import Formulario from "./components/Formulario";
 function App() {
+  const [citas, guardarCitas] = useState([]);
+
+  //funcion que tome las citas actuales y agrege la nueva.
+  const crearCita = (cita) => {
+    guardarCitas([...citas, cita]);
+  };
+
   return (
     <div className="App">
-      <h1>Administrador de Citas</h1>
+      <Fragment>
+        <h1>Administrador de Pacientes</h1>
+        <div className="container">
+          <div className="row">
+            <div className="one-half column">
+              <Formulario crearCita={crearCita} />
+            </div>
+            <div className="one-half column">2</div>
+          </div>
+        </div>
+      </Fragment>
     </div>
   );
 }
 
 export default App;
-c
---
+++ src/components/Formulario.js
@@ -0,0 +1,114 @@
+import React, { Fragment, useState } from "react";
+import uuid from "uuid/dist/v4";
+
+const Formulario = ({ crearCita }) => {
+  //State de las citas
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+  //State de los errores de validacion
+  const [error, actualizarError] = useState(false);
+
+  //Funcion que se ejecuta cada que el usuario escribe en un input.
+  const actualizarState = (e) => {
+    actualizarCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
+  //extraer los valores.
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  //Cuando se pulsa el boton de Agregar Cita
+  const submitCita = (e) => {
+    e.preventDefault();
+
+    //validar
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      actualizarError(true);
+      return;
+    }
+    actualizarError(false);
+
+    //asignar ID.
+    cita.id = uuid();
+
+    //Crear la Cita.
+    crearCita(cita);
+
+    // Resetear el formulario
+    actualizarCita({
+      mascota: "",
+      propietario: "",
+      fecha: "",
+      hora: "",
+      sintomas: "",
+    });
+  };
+  return (
+    <Fragment>
+      <h2>Crear Cita</h2>
+      {error ? (
+        <p className="alerta-error">Todos los campo son obligatorios</p>
+      ) : null}
+      <form onSubmit={submitCita}>
+        <label>Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+          onChange={actualizarState}
+          value={mascota}
+        />
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre Dueño de la Mascota"
+          onChange={actualizarState}
+          value={propietario}
+        />
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
+        <label>Sintomas </label>
+        <textarea
+          className="u-full-width"
+          name="sintomas"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Cita
+        </button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default Formulario;
